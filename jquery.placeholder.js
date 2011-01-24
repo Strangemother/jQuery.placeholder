30472f1834232e7542222318905764d19cfab17d
Jay Jagpal <jay@strangemother.com>
2011-01-24 04:10:27 +0000

    added automatic form binding to clear placeholder on submission. fixed overtyping bug. At version 0.2.1

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,7 +1,13 @@
 /**
  * 
  * @Author: Jay Jagpal
- * @Version: 0.1.1
+ * @Version: 0.2.1
+ * 
+ * Update: Fix over type bug - When a user types the same 
+ * value as the placeholder text.
+ * 
+ * Update: Automatic form handling - clearing text if the 
+ * element is within a form without a change.
  * 
  */
 
@@ -10,7 +16,7 @@ page = this;
 (function( $ ){
 
   $.fn.placeholder = function() {
-
+      var __version__ = 'v0.2'
       var _overridden = false;
       var _defaultValue = 'Type Here'
       var placeholderValue = null;
@@ -20,7 +26,10 @@ page = this;
       var color = '#CFCFCF'
       
       var self = this;
-      
+      /*
+       * Applied in .2 to allow form handling and checking.
+       */
+      var hasForm = false
       var _placeholderSupported = null;
       var _hasPlaceholder = null;
       
@@ -77,26 +86,68 @@ page = this;
                * 
                */
         }
+        
+        /**
+         * Try to find any forms on the 
+         * page.
+         */
+        var forms = $('form')
+        
+        /*
+         * Read eaxh form and determine if this
+         * element is within any of the forms collected
+         * on the page.
+         */
+        for( var i=0; i < forms.length; i++)
+        {
+          var form = forms[i]
+          var formElement = $(form).has(this)
+          
+          /**
+           * If any forms have this element,
+           * bind a submittion handler to the form to
+           * capture the event before the data is sent.
+           */
+          if(formElement.length > 0)
+          {
+            for(var j=0; j < formElement.length; j++)
+            {
+              
+              self.hasForm = true
+              
+              /*
+               * If the form is sumbitted, clear the
+               * text to ensure placeholder text is
+               * not sent with the form submittion.
+               */
+              $(formElement[i]).submit(function(){
+                self.removePlaceholderText()
+              });
+              
+            }
+          } 
+        }
       }
       
       
          /*
          * set the arguments passed to the corresponding 
-         * variables 
+         * variables. This allows the user to send the
+         * a specific order. 
          */
       this.parseArguments = function(args) {
         
         switch(args.length)
         {
           case 0:
-            suggestedPlaceholder = _defaultValue
+            self.setPlaceHolder(_defaultValue)
             break;
           case 3:
             color = args[2]
           case 2:
             if(typeof(args[1]) == 'string')
             {
-              suggestedPlaceholder = args[1]
+              self.setPlaceHolder(args[1])
             }
             else if(typeof(args[1]) == 'boolean')
             {
@@ -106,7 +157,7 @@ page = this;
           case 1:
             if(typeof(args[0]) == 'string')
             {
-              suggestedPlaceholder = args[0]
+              self.setPlaceHolder(args[0])
             }
             else if(typeof(args[0]) == 'boolean')
             {
@@ -116,6 +167,14 @@ page = this;
             break;
         }
  
+      }
+      
+      /*
+       * 0.2.1 - helps in fixing the overtype bug. not many users
+       * will type this combination of characters.
+       */
+      this.setPlaceHolder = function(){
+        suggestedPlaceholder = String(arguments[0]).replace(' ', String.fromCharCode(160)) + String.fromCharCode(160) + "" + String.fromCharCode(160) + " ";
       }
       
       /**
@@ -177,7 +236,8 @@ page = this;
         
       }
       
-      this.addPlaceholderText = function() {
+    
+     this.addPlaceholderText = function() {
         
         var el = self;
         
@@ -192,7 +252,6 @@ page = this;
           
           if(arguments[0])
               text = arguments[0]
-          
           this.css('color', color)
           this.val(text)
           this.addClass('placeholder')
@@ -202,7 +261,6 @@ page = this;
       
       this.removePlaceholderText = function() {
         var el = self;
-        
         if(arguments[0])
           el = arguments[0]
         
@@ -223,10 +281,40 @@ page = this;
         self.removePlaceholderText()
       }
       
+      this.hasCSSStyle = function() {
+       
+        
+        var rule = '.placeholder'
+        if(arguments[0])
+          rule = arguments[0]
+          
+        var sheets = document.styleSheets
+        
+        /*
+         * Walk through each sheet
+         */
+        for(var i=0; i < sheets.length; i++)
+        {
+          /*
+           * Walk through each rule of this stylesheet
+           */
+          for(var j=0; j < sheets[i].cssRules.length; j++)
+          {
+            
+            var cssRule = sheets[i].cssRules[j].selectorText;
+            if (rule.indexOf(cssRule) != -1)
+            {
+              console.log(cssRule)
+            }
+          }
+        }
+      }
+      
       /**
        * initiate the intial (construct)
        */
       this.init(arguments)
+      //this.hasCSSStyle()
 
   };
 })( jQuery );
